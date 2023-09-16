ebb2d999ab8360230addab21e53e227ef5881333
Alastors <thensa107@gmail.com>
2023-09-16 14:39:26 -0400

    server

--- src/main/java/thaumicboots/api/network/PacketSpeedToggleAck.java
+++ src/main/java/thaumicboots/api/network/PacketSpeedToggleAck.java
@@ -6,6 +6,8 @@ import net.minecraft.item.ItemStack;
 import cpw.mods.fml.common.network.simpleimpl.IMessage;
 import cpw.mods.fml.common.network.simpleimpl.IMessageHandler;
 import cpw.mods.fml.common.network.simpleimpl.MessageContext;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
 import io.netty.buffer.ByteBuf;
 import thaumicboots.api.ItemBoots;
 import thaumicboots.main.utils.compat.GTNHLibHelper;
@@ -25,6 +27,7 @@ public class PacketSpeedToggleAck implements IMessage, IMessageHandler<PacketSpe
     }
 
     @Override
+    @SideOnly(Side.CLIENT)
     public IMessage onMessage(PacketSpeedToggleAck message, MessageContext ctx) {
         Minecraft mc = Minecraft.getMinecraft();
         final ItemStack boots = ItemBoots.getBoots(mc.thePlayer);
