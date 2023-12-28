c945d165f56e9e9aab414af17b28144720b0afee
Alastors <78517796+Alastors@users.noreply.github.com>
2023-12-28 00:11:27 -0500

    prevents the crash itself
    TODO: still crashes on modifier toggle

--- src/main/java/thaumicboots/events/BootsEventHandler.java
+++ src/main/java/thaumicboots/events/BootsEventHandler.java
@@ -85,8 +85,13 @@ public class BootsEventHandler {
         if (item instanceof IMeteor meteor && (player.isSneaking())) {
             meteor.specialEffect2(event);
         } else if (item instanceof ItemBoots boots) {
-            event.entityLiving.motionY += (boots.getJumpModifier()
-                    * ItemBoots.isJumpEnabled(player.inventory.armorItemInSlot(0)));
+            double modifier;
+            try {
+                modifier = ItemBoots.isJumpEnabled(player.inventory.armorItemInSlot(0));
+            } catch (NullPointerException e) {
+                modifier = 1;
+            }
+            event.entityLiving.motionY += (boots.getJumpModifier() * modifier);
         }
         // 0.275D is approx 3 blocks, 0.265D will get you to just 3 blocks,
         // 0.55D is approx 5.5 blocks, so 0.275 is around 2.25 additional blocks
