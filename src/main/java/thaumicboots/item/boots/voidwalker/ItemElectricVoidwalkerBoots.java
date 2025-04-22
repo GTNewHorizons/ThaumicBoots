28be5cfe5e23dbfb3b9a82d572519b240b990f8c
__felix__ <134875831+felixfour@users.noreply.github.com>
2025-04-22 12:59:13 -0400

    update modid

--- src/main/java/thaumicboots/item/boots/voidwalker/ItemElectricVoidwalkerBoots.java
+++ src/main/java/thaumicboots/item/boots/voidwalker/ItemElectricVoidwalkerBoots.java
@@ -23,7 +23,7 @@ import thaumcraft.common.items.armor.Hover;
 import thaumicboots.api.ItemElectricBoots;
 import thaumicboots.main.utils.TabThaumicBoots;
 
-@Optional.Interface(iface = "gregtech.api.hazards.IHazardProtector", modid = "dreamcraft")
+@Optional.Interface(iface = "gregtech.api.hazards.IHazardProtector", modid = "gregtechNH")
 public class ItemElectricVoidwalkerBoots extends ItemElectricBoots
         implements IWarpingGear, ISpecialArmor, IHazardProtector {
 
@@ -121,7 +121,7 @@ public class ItemElectricVoidwalkerBoots extends ItemElectricBoots
     }
 
     @Override
-    @Optional.Method(modid = "dreamcraft")
+    @Optional.Method(modid = "gregtechNH")
     public boolean protectsAgainst(ItemStack itemStack, Hazard hazard) {
         return true;
     }
