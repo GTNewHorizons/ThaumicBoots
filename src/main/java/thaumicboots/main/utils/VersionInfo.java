ee4431c783265a847499a6764de50c58d38126d6
Maya <10861407+serenibyss@users.noreply.github.com>
2025-04-23 14:56:46 -0500

    update

--- src/main/java/thaumicboots/main/utils/VersionInfo.java
+++ src/main/java/thaumicboots/main/utils/VersionInfo.java
@@ -1,9 +1,11 @@
 package thaumicboots.main.utils;
 
+import thaumicboots.TBTags;
+
 public class VersionInfo {
 
-    public static final String ModID = "GRADLETOKEN_MODID";
-    public static final String ModName = "GRADLETOKEN_MODNAME";
-    public static final String Version = "GRADLETOKEN_VERSION";
+    public static final String ModID = "thaumicboots";
+    public static final String ModName = "Thaumic Boots";
+    public static final String Version = TBTags.VERSION;
     public static final String Depends = "required-after:Thaumcraft; ";
 }
